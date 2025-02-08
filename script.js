1120119b85185bf06df732e4eee10ce474cc28ec
Focus-001 <gldfocusss@gmail.com>
2025-02-08 15:06:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,36 +2,51 @@
 function changerTheme(theme) {
     // Supprimer toutes les classes de thème existantes
     document.body.classList.remove(
-        'theme-hacking',
-        'theme-rose',
-        'theme-bleu-rouge',
-        'theme-neon',
-        'theme-spacial',
-        'theme-sakura'
+      'theme-hacking', 'theme-rose', 'theme-bleu-rouge', 'theme-neon', 'theme-spacial', 'theme-sakura'
     );
-
-    // Ajouter la classe correspondant au thème choisi
+    // Ajouter la classe correspondante au thème choisi
     document.body.classList.add('theme-' + theme);
-}
-
-// Fonction pour afficher/masquer le menu des paramètres
-function toggleSettingsMenu() {
-    const menu = document.getElementById('settings-menu');
-    menu.classList.toggle('open');
-}
-
-// Fonction pour afficher la popup des thèmes
-function toggleThemePopup() {
+  }
+  
+  // Fonction pour afficher/masquer le menu des paramètres
+  function toggleSettingsMenu() {
+    const themeButton = document.querySelector('.btn-theme');
+    const communityButton = document.querySelector('.btn-communaute');
+    
+    // Afficher/masquer les boutons Thème et Communauté
+    if (themeButton.style.display === 'none') {
+      themeButton.style.display = 'block';
+      communityButton.style.display = 'block';
+    } else {
+      themeButton.style.display = 'none';
+      communityButton.style.display = 'none';
+    }
+  }
+  
+  // Fonction pour afficher la popup des thèmes
+  function toggleThemePopup() {
     const popup = document.getElementById('theme-popup');
     popup.classList.toggle('open');
-
-    // Masquer le menu des paramètres quand la popup des thèmes est ouverte
-    const menu = document.getElementById('settings-menu');
-    menu.classList.remove('open');
-}
-
-// Fonction pour fermer la popup
-function closePopup() {
+  }
+  
+  // Fonction pour afficher la popup Communauté
+  function toggleCommunityPopup() {
+    const popup = document.getElementById('community-popup');
+    popup.classList.toggle('open');
+    // Masquer la popup des thèmes si elle est ouverte
+    const themePopup = document.getElementById('theme-popup');
+    themePopup.classList.remove('open');
+  }
+  
+  // Fonction pour fermer la popup
+  function closePopup() {
     const popup = document.getElementById('theme-popup');
     popup.classList.remove('open');
-}
+  }
+  
+  // Fonction pour fermer la popup communauté
+  function closeCommunityPopup() {
+    const popup = document.getElementById('community-popup');
+    popup.classList.remove('open');
+  }
+  
